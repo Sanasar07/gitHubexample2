c71763fd0dac315c24b7ba2ce2ed0d130f14efc3
Саносар Кркикян <krkikyans@bk.ru>
2024-08-17 17:51:07 +0300

    добавление функции генератора пароля

--- Program.cs
+++ Program.cs
@@ -8,13 +8,36 @@ namespace gitHubexample2
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static Random random = new Random();
+
+        static string GeneratePassword(int length, string alph)
         {
-            HelloWorld();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < length; i++)
+            {
+                sb.Append(alph[random.Next(alph.Length)]);
+            }
+            return sb.ToString();
         }
 
-        static void HelloWorld()
+        static void RunPasswordGenrationCLI()
         {
-            Console.WriteLine("Hello World");
+            try
+            {
+                Console.Write("Введите длину пароля: ");
+                int length = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите алфавит: ");
+                string alph = Console.ReadLine();
+                string password = GeneratePassword(length, alph);
+                Console.WriteLine($"Сгенерированный пароль: {password}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
         }
-    }
+
+        static void Main(string[] args)
+        {
+            RunPasswordGenrationCLI();
+        }
